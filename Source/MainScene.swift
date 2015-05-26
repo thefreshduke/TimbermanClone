ba4fa1321fe52e5c396f90ff3efe7fd06b81acca
thefreshduke <scottyshaw1@gmail.com>
2015-05-26 22:14:38 +0100

    Tree completed; add branches next

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -1,43 +1,84 @@
 import Foundation
 
 class MainScene : CCNode {
+    var _base : CCSprite!
     var _character : CCSprite!
     var _restartButton : CCButton!
     var _scoreLabel : CCLabelTTF!
     var _timerBar : CCSprite!
     var _view : CCGLView!
     
+    var _doesBranchExist : Bool = false
     var _isBranchLeft : Bool = false
     var _isCharacterLeft : Bool = true
     var _score : NSInteger = 0
-    var _timer : Double = 5.0
+    var _timer : Float = 5.0
+    var screenDimensions : CGRect = UIScreen.mainScreen().bounds
+    var screenHeight : CGFloat = UIScreen.mainScreen().bounds.size.height
+    var screenWidth : CGFloat = UIScreen.mainScreen().bounds.size.width
     
-    func didLoadFromCCB() {
-        prepareNewGame()
+    var widthMidpoint : CGFloat = 0.5
+    var timerBonus : Float = 0.25
+    var timerStart : Float = 5.0
+    var timerMax : Float = 10.0
+    
+    func didLoadFromCCB () {
+        _base.position = CGPoint(x : widthMidpoint, y : 0.0)
+        buildTree(_base.contentSize.height)
+        resetEnvironment()
+    }
+    
+    func buildTree (elevation : CGFloat) {
+        if elevation < screenDimensions.size.height {
+            let tree = CCBReader.load("TreePiece")
+            _base.addChild(tree)
+            tree.position = CGPoint(x: _base.contentSize.width / 2.0, y: elevation)
+            buildTree(elevation + tree.contentSize.height)
+        }
+    }
+    
+    // prepare new game
+    func resetEnvironment () {
+        _character.position.x = 0.0
+        _character.flipX = false
+        _isCharacterLeft = true
+        _restartButton.visible = false
+        _score = 0
+        _scoreLabel.position.x = widthMidpoint
+        _timer = timerStart
+        _timerBar.position.x = widthMidpoint
+        _timerBar.scaleX = timerStart / timerMax
+        self.userInteractionEnabled = true
+        // add branches to the tree; no branch at lowest tree piece ???
     }
     
     // automatic
     override func update (delta: CCTime) {
-        if (_score > 0) {
-            _timer -= delta
-            if (_timer <= 0.0) {
+        
+        // the game has started
+        if _score > 0 {
+            _timer -= Float(delta)
+            
+            // time ran out
+            if _timer <= 0.0 {
                 endGame()
             }
         }
         
-        _timerBar.scaleX = Float(_timer / 10.0)
+        _timerBar.scaleX = Float(_timer / timerMax)
         _scoreLabel.string = String(_score)
     }
     
     // process user input
     override func touchBegan (touch : CCTouch!, withEvent event : CCTouchEvent!) {
+        
         // each tap moves the player
         updatePlayer(touch)
         
         // check for body shot (switch into death)
         checkLoseConditions()
         
-        //only occurs if the player avoided the switch into death
+        //only occurs if the player avoided a body shot
         updateStats()
         updateTree ()
         
@@ -47,16 +88,15 @@ class MainScene : CCNode {
     
     // move and flip character
     func updatePlayer (touch : CCTouch!) {
-        let tapPoint = touch.locationInView(self._view)
-        var screenDimensions : CGRect = UIScreen.mainScreen().bounds
-        var screenWidth : CGFloat = screenDimensions.size.width
+        let tapPoint : CGFloat = touch.locationInView(self._view).x / screenWidth
         
         // left tap
-        if (tapPoint.x < screenWidth / 2.0) {
+        if tapPoint < widthMidpoint {
             _character.position.x = 0.0
             _character.flipX = false
             _isCharacterLeft = true
         }
+            
         // right tap
         else {
             _character.position.x = (screenWidth - _character.contentSize.width) / screenWidth
@@ -68,7 +108,7 @@ class MainScene : CCNode {
     // increase score and timer
     func updateStats () {
         _score += 1
-        _timer = _timer + 0.25 > 10.0 ? 10.0 : _timer + 0.25
+        _timer = _timer + timerBonus > timerMax ? timerMax : _timer + timerBonus
     }
     
     // move branches down
@@ -76,32 +116,17 @@ class MainScene : CCNode {
         // move branches down
     }
     
+    // verify if the character and a branch collided
     func checkLoseConditions () {
-        if (_isCharacterLeft && _isBranchLeft) {
+        if _isCharacterLeft && _isBranchLeft {
             endGame()
         }
     }
     
-    // freeze everything
+    // freeze environment
     func endGame () {
         _timer = 0.0
         self.userInteractionEnabled = false
         _restartButton.visible = true
-    }
-    
-    // reset everything
-    func prepareNewGame () {
-        _character.position.x = 0.0
-        _character.flipX = false
-        _isCharacterLeft = true
-        _restartButton.visible = false
-        _score = 0
-        _scoreLabel.position.x = 0.5
-        _timer = 5.0
-        _timerBar.position.x = 0.5
-        _timerBar.scaleX = 0.5
-        self.userInteractionEnabled = true
-        // new tree and branches
-        // no branch at lowest tree piece
     }
 }
