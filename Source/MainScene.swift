8cdfa54efba4567e14227b8d76fff9b1be39a8e9
thefreshduke <scottyshaw1@gmail.com>
2015-05-26 11:49:16 +0100

    Added comments; other minor edits

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -7,6 +7,8 @@ class MainScene : CCNode {
     var _timerBar : CCSprite!
     var _view : CCGLView!
     
+    var _isBranchLeft : Bool = false
+    var _isCharacterLeft : Bool = true
     var _score : NSInteger = 0
     var _timer : Double = 5.0
     
@@ -14,6 +16,7 @@ class MainScene : CCNode {
         prepareNewGame()
     }
     
+    // automatic
     override func update (delta: CCTime) {
         if (_score > 0) {
             _timer -= delta
@@ -26,45 +29,71 @@ class MainScene : CCNode {
         _scoreLabel.string = String(_score)
     }
     
+    // process user input
     override func touchBegan (touch : CCTouch!, withEvent event : CCTouchEvent!) {
+        // each tap moves the player
         updatePlayer(touch)
-        // body shot end game check
+        
+        // check for body shot (switch into death)
+        checkLoseConditions()
+        
+        //only occurs if the player avoided the switch into death
         updateStats()
-//        updateTree () {
-            // move branches down
-//        }
-        // head shot end game check
+        updateTree ()
+        
+        // check for head shot (head on death or switch into head on death)
+        checkLoseConditions()
     }
     
+    // move and flip character
     func updatePlayer (touch : CCTouch!) {
         let tapPoint = touch.locationInView(self._view)
         var screenDimensions : CGRect = UIScreen.mainScreen().bounds
         var screenWidth : CGFloat = screenDimensions.size.width
         
+        // left tap
         if (tapPoint.x < screenWidth / 2.0) {
             _character.position.x = 0.0
             _character.flipX = false
+            _isCharacterLeft = true
         }
+        // right tap
         else {
             _character.position.x = (screenWidth - _character.contentSize.width) / screenWidth
             _character.flipX = true
+            _isCharacterLeft = false
         }
     }
     
+    // increase score and timer
     func updateStats () {
         _score += 1
         _timer = _timer + 0.25 > 10.0 ? 10.0 : _timer + 0.25
     }
     
+    // move branches down
+    func updateTree () {
+        // move branches down
+    }
+    
+    func checkLoseConditions () {
+        if (_isCharacterLeft && _isBranchLeft) {
+            endGame()
+        }
+    }
+    
+    // freeze everything
     func endGame () {
         _timer = 0.0
         self.userInteractionEnabled = false
         _restartButton.visible = true
     }
     
+    // reset everything
     func prepareNewGame () {
         _character.position.x = 0.0
         _character.flipX = false
+        _isCharacterLeft = true
         _restartButton.visible = false
         _score = 0
         _scoreLabel.position.x = 0.5
@@ -73,5 +102,6 @@ class MainScene : CCNode {
         _timerBar.scaleX = 0.5
         self.userInteractionEnabled = true
         // new tree and branches
+        // no branch at lowest tree piece
     }
 }
